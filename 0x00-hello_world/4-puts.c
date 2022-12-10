de8ffdfb28070d5d6b93574268cd43346aeab3f1
De3an <deantend@gmail.com>
2022-12-09 19:36:01 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main-Entry Point
-*Return '0' always.
+* main - Entry Point
+*Return: 0 (Success)
 */
-int main (void)
+int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
