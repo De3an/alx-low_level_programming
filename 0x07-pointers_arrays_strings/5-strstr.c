275077513d7bfa524fd49f007f422f3323caa918
De3an <deantend@gmail.com>
2023-01-02 19:28:20 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
-/*_strstr - a function that locates a substring
+/*
+ * _strstr - a function that locates a substring
  * @haystack: input string to search for matching substrings
  * @needle: subtring to search for
  * Return: a pointer to the beginning of the located substring or
@@ -7,7 +8,8 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	/* we initialize a helping variable
+	/*
+	 * we initialize a helping variable
 	 * to assist in returning one of
 	 * our parameters pointers haystack
 	 */
